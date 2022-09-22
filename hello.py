cd9b5618cfb8cf28a84cb7d30eaa4b2fd846707e
BRC_Del <delspcsolutions@outlook.com>
2022-09-22 17:08:05 +0100

    playing with random

--- hello.py
+++ hello.py
@@ -1,7 +1,13 @@
 from flask import Flask
+import random
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-	return 'lmao hi'
+	x =  random.randint(1,100)
+	if x % 2 == 0:
+		return 'even'
+	else:
+		return 'odd'
+	return 'done'
 
