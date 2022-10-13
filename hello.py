da5f6692c3dec8aaba6330fccd0bf70e3a71478d
BRC_Del <delspcsolutions@outlook.com>
2022-10-13 16:53:36 +0100

    account app routes

--- hello.py
+++ hello.py
@@ -1,15 +1,39 @@
-from flask import Flask, abort
+from flask import Flask, abort, url_for, request
 import random
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def home():
 	x =  random.randint(1,100)
-	if x % 2 == 0:
-		return 'even'
+	return str(x)
+
+@app.route('/static/vmask')
+def static_example_img():
+	start = '<img src="'
+	url = url_for("static", filename="vmask.jpg")
+	end = '">'
+	return start+url+end, 200
+
+@app.route("/account", methods=['GET', 'POST'])
+def account():
+	if request.method == 'POST':
+		print(request.form)
+		name = request.form['name']
+		return "Hello %s" % name
 	else:
-		return 'odd'
-	return 'done'
+		page = '''
+		<html><body>
+			<form action="" method="post" name="form">
+				<label form="name">Name:</label>
+				<input type="text" name="name" id="name"/>
+				<input type="submit" name="submit" id="submit"/>
+			</form>
+		</body></html>'''
+		return page
+
+@app.route("/account/<name>")
+def accountName(name):
+	return "Hello %s" % name
 
 @app.route('/404')
 def force404():
@@ -17,7 +41,7 @@ def force404():
 
 @app.errorhandler(404)
 def page_not_found(error):
-	return "Whatever it is you're looking for, I don't have it.", 404
+	return "Whatever it is you're looking for, I don't have it. (Error 404: Not Found)", 404
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', debug=True)
