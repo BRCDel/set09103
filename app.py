257e6b0fda8b1f5f8edc2b6c7ca7f3a046e3aa27
Del <delspcsolutions@outlook.com>
2022-11-17 16:06:30 +0000

    first commit

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ def init(app):
 		print("INIT FUNCTION")
 		config_location = 'etc/defaults.cfg'
 		config.read(config_location)
-		#app.config.from_object('etc/defaults.default_settings')
 
 		app.config['DEBUG'] = config.get("config", "debug")
 		app.config['ip_address'] = config.get("config", "ip_address")
@@ -21,110 +20,8 @@ def init(app):
 init(app)
 
 @app.route('/')
-def home():
-	x = random.randint(1,100)
-	return "Here's a random number: "+str(x)
-
-@app.route('/config/')
-def config():
-	s = []
-	s.append('Config info follows')
-	s.append('debug: '+str(app.config['DEBUG']))
-	s.append('port: '+app.config['port'])
-	s.append('url: '+app.config['url'])
-	s.append('ip_address: '+app.config['ip_address'])
-	return ', '.join(s)
-
-
-@app.route('/session/')
-def index():
-    return "Root route for the sessions example"
-
-@app.route('/session/write/<name>/')
-def write(name=None):
-    session['name'] = name
-    return "Wrote %s into 'name' key of session" % name
-
-@app.route('/session/read/')
-def read():
-    try:
-      if(session['name']):
-          return str(session['name'])
-    except KeyError:
-      pass
-    return "No session variable set for 'name' key"
-
-@app.route('/session/remove/')
-def remove():
-    session.pop('name', None)
-    return "Removed key 'name' from session"
-
-@app.route('/login/')
-@app.route('/login/<message>/')
-def login(message=None):
-	if (message != None):
-		flash(message)
-	else:
-		flash('A default message')
-	return redirect(url_for('login_template'))
-
-@app.route('/login_template/')
-def login_template():
-	return render_template('login.html')
-
-@app.route('/inherits/')
-def inherits():
-	return render_template('base.html')
-
-@app.route('/inherits/one')
-def inherits_one():
-	return render_template('extension.html')
-
-@app.route('/inherits/two')
-def inherits_two():
-	return render_template('second_extension.html')
-
-@app.route('/users')
-def users():
-	names = ['del', 'v', 'david', 'maine', 'rebb']
-	return render_template('users.html', names=names)
-
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-	return render_template('hello.html', name=name)
-
-@app.route('/static/vmask')
-def static_example_img():
-	start = '<img src="'
-	url = url_for("static", filename="vmask.jpg")
-	end = '">'
-	return start+url+end, 200
-
-@app.route("/account", methods=['GET', 'POST'])
-def account():
-	if request.method == 'POST':
-		print(request.form)
-		name = request.form['name']
-		return "Hello %s" % name
-	else:
-		page = '''
-		<html><body>
-			<form action="" method="post" name="form">
-				<label form="name">Name:</label>
-				<input type="text" name="name" id="name"/>
-				<input type="submit" name="submit" id="submit"/>
-			</form>
-		</body></html>'''
-		return page
-
-@app.route("/account/<name>")
-def accountName(name):
-	return "Hello %s" % name
-
-@app.route("/add/<int:first>/<int:second>")
-def add(first,second):
-	return str(first+second)
+def home()
+	return "Home page here"
 
 @app.route('/404')
 def force404():
