8edea3e699c0ef0d7ad3c81240a1520a6aa05feb
BRCDel <delspcsolutions@outlook.com>
2022-12-05 14:58:45 +0000

    separate init db script v1

--- prepare_db.py
+++ prepare_db.py
@@ -4,4 +4,4 @@ import sqlite3
 init_db()
 #feed sample data into db, will do this later
 #    with app.open_resource('prepare.sql', mode='r') as load:
-#        db.cursor().executescript(load.read())
+#        db.cursor().executescript(load.read()) 
