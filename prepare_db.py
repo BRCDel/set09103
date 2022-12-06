911e74d37f9551b5f7bb4e1923c6352c15fe2c59
BRCDel <delspcsolutions@outlook.com>
2022-12-06 16:19:17 +0000

    if this works I'm gonna get mad

--- prepare_db.py
+++ prepare_db.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 conn = sqlite3.connect('db/core.db')
-conn.cursor().executescript('db/schema.sql')
+conn.cursor().executescript("db/schema.sql")
 #feed sample data into db, will do this later
 #    with app.open_resource('prepare.sql', mode='r') as load:
 #        db.cursor().executescript(load.read()) 
