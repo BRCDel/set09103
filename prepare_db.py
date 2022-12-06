08159361387b9e60e8ba688cfb357cad42c3b8d4
BRCDel <delspcsolutions@outlook.com>
2022-12-06 18:42:57 +0000

    cleanup prepare_db file

--- prepare_db.py
+++ prepare_db.py
@@ -1,12 +1,8 @@
 import sqlite3
 
-
 with open('db/schema.sql', mode='r') as schema:
     script = schema.read()
 conn = sqlite3.connect('db/core.db')
 conn.cursor().executescript(script)
 conn.commit()
 conn.close()
-#feed sample data into db, will do this later
-#    with app.open_resource('prepare.sql', mode='r') as load:
-#        db.cursor().executescript(load.read()) 
