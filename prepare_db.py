7c9019dd8fb85b745c60b31625ae5353b361bd87
BRCDel <delspcsolutions@outlook.com>
2022-12-06 16:06:52 +0000

    rewrite prepare_db.py

--- prepare_db.py
+++ prepare_db.py
@@ -1,7 +1,8 @@
-from datastore import init_db
 import sqlite3
 
-init_db()
+conn = sqlite3.connect('db/core.db')
+with app.open_resource('db/schema.sql', mode='r') as f:
+    conn.cursor().executescript(f.read())
 #feed sample data into db, will do this later
 #    with app.open_resource('prepare.sql', mode='r') as load:
 #        db.cursor().executescript(load.read()) 
