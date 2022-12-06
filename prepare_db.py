41fb5226ee0a7bfa0539c6b445cc14fccd2549e0
BRCDel <delspcsolutions@outlook.com>
2022-12-06 16:25:04 +0000

    the answer is yes

--- prepare_db.py
+++ prepare_db.py
@@ -1,7 +1,8 @@
 import sqlite3
 
 conn = sqlite3.connect('db/core.db')
-conn.cursor().executescript('db/schema.sql'.read())
+with open('db/schema.sql', mode='r') as script:
+    conn.cursor().executescript(script.read())
 #feed sample data into db, will do this later
 #    with app.open_resource('prepare.sql', mode='r') as load:
 #        db.cursor().executescript(load.read()) 
