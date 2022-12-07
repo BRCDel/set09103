7d05e193a4852a4574df404a494e9bb9824f6f93
BRCDel <delspcsolutions@outlook.com>
2022-12-07 15:58:54 +0000

    separate query variable, trying cast to int for id

--- app.py
+++ app.py
@@ -78,10 +78,11 @@ def home():
 #        print(list.values())
 #        print(list.keys())
         part_list_keys = list(part_list)
-        print(part_list_keys)
+#        print(part_list_keys)
         for i in range(2, 10):
             print(part_list_keys[i])
-#            part_list[i] = cur.execute("SELECT FROM " + part_list_keys[i] + "s WHERE id = " + part_list[i] + ";" )
+            query = "SELECT FROM " + part_list_keys[i] + "s WHERE id = " + part_list[i] + ";"
+            part_list[i] = cur.execute(query)
 #        print(part_list)
     return render_template('index.html', lists=user_made_parts_lists)
 
