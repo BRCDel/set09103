403e329185e9811c0ed35f4052b1a435f48d04e4
BRCDel <delspcsolutions@outlook.com>
2022-12-07 20:38:01 +0000

    hopefully db connection in global scope

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ db_location = "db/core.db"
 
 def init(app):
     config = configparser.ConfigParser()
-    conn = connect_db()
-    lists_table_statement="""CREATE TABLE lists ( id integer PRIMARY KEY, username text NOT NULL, json TEXT NOT NULL )"""
-    create_table(conn, lists_table_statement)
+    db = connect_db()
+    db.row_factory = sqlite3.Row
+    cur = db.cursor()
 
     try:
         config_location = 'etc/defaults.cfg'
